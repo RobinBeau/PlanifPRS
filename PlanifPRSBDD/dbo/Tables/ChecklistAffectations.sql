d3a83d570b1fbbd1717a6cf286841373fcadcc46
RobinBeau <fumbi3726@hotmail.fr>
2025-09-23 15:48:23 +0200

    ajout bdd à jour

---
+++ PlanifPRSBDD/dbo/Tables/ChecklistAffectations.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[ChecklistAffectations] (
+    [Id]              INT            IDENTITY (1, 1) NOT NULL,
+    [ChecklistId]     INT            NOT NULL,
+    [UtilisateurId]   INT            NULL,
+    [GroupeId]        INT            NULL,
+    [TypeAffectation] NVARCHAR (20)  NOT NULL,
+    [DateAffectation] DATETIME       DEFAULT (getdate()) NOT NULL,
+    [AffectePar]      NVARCHAR (100) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [CK_ChecklistAffectations_UserOrGroup] CHECK ([UtilisateurId] IS NOT NULL AND [GroupeId] IS NULL OR [UtilisateurId] IS NULL AND [GroupeId] IS NOT NULL),
+    FOREIGN KEY ([ChecklistId]) REFERENCES [dbo].[PRS_Checklist] ([Id]),
+    FOREIGN KEY ([GroupeId]) REFERENCES [dbo].[GroupesUtilisateurs] ([Id]),
+    FOREIGN KEY ([UtilisateurId]) REFERENCES [dbo].[Utilisateurs] ([Id])
+);
+
---
+++ PlanifPRSBDD/dbo/Tables/HistoriqueEdit.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[HistoriqueEdit] (
+    [Id]            INT            IDENTITY (1, 1) NOT NULL,
+    [PrsId]         INT            NOT NULL,
+    [Action]        NVARCHAR (30)  NOT NULL,
+    [AncienStatut]  NVARCHAR (50)  NULL,
+    [NouveauStatut] NVARCHAR (50)  NOT NULL,
+    [UserLogin]     NVARCHAR (100) NOT NULL,
+    [DateAction]    DATETIME2 (7)  NOT NULL,
+    [Changements]   NVARCHAR (MAX) NOT NULL,
+    CONSTRAINT [PK_HistoriqueEdit] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_HistoriqueEdit_Prs] FOREIGN KEY ([PrsId]) REFERENCES [dbo].[PRS] ([Id])
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_HistoriqueEdit_PrsId]
+    ON [dbo].[HistoriqueEdit]([PrsId] ASC);
+
